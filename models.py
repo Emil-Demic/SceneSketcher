40251c8b911e6971a7cd571bb330c4672329c937
Emil-Demic <77987406+Emil-Demic@users.noreply.github.com>
2024-08-12 20:40:11 +0200

    plz 2

--- models.py
+++ models.py
@@ -33,16 +33,11 @@ class GCNAttention(nn.Module):
         '''
         label_list = label_list[0]
         gcn_input = torch.zeros((num_categories, LOOP_NUM, 2052), dtype=torch.float32, requires_grad=False).cuda()
-        if numel(label_list[0]) > 0:
+        if len(label_list[0].size()) > 0:
             img_features = self.image_bbox_extract_net(image_list[0])
             # print(img_features.shape)
             # print(category_list[0].shape)
-            try:
-                full_features = torch.hstack((img_features, category_list[0])).cuda()
-            except:
-                print(label_list[0])
-                print(img_features)
-                print(category_list[0])
+            full_features = torch.hstack((img_features, category_list[0])).cuda()
             category_count = np.zeros(num_categories, dtype=np.int32)
             for i, tmp_feature in enumerate(full_features):
                 # plt.imshow(image_list[0][i].permute(1,2,0))
