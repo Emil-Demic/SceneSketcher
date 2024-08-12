b61e860cedbdfb8a2ec86b6547a9b9f24138ade3
Emil-Demic <77987406+Emil-Demic@users.noreply.github.com>
2024-08-12 20:18:34 +0200

    bug check

--- models.py
+++ models.py
@@ -4,6 +4,7 @@
 import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
+from torch import numel
 
 from layers import GraphConvolution
 from utils_model import get_network
@@ -35,8 +36,8 @@ class GCNAttention(nn.Module):
         label_list = label_list[0]
         gcn_input = torch.zeros((num_categories, LOOP_NUM, 2052), dtype=torch.float32, requires_grad=False).cuda()
         img_features = self.image_bbox_extract_net(image_list[0])
-        print(img_features.shape)
-        print(category_list[0].shape)
+        if (numel(img_features) == 0):
+            print("error")
         full_features = torch.hstack((img_features, category_list[0])).cuda()
         category_count = np.zeros(num_categories, dtype=np.int32)
         for i, tmp_feature in enumerate(full_features):
