68e04224f22db91dd0fd184b933944b94585f3f8
Emil-Demic <77987406+Emil-Demic@users.noreply.github.com>
2024-08-21 18:22:03 +0200

    plz resnext

--- layers.py
+++ layers.py
@@ -1,6 +1,7 @@
 import math
 
 import torch
+from torch.nn import Linear
 from torch.nn.parameter import Parameter
 from torch.nn.modules.module import Module
 
@@ -14,11 +15,12 @@ class GraphConvolution(Module):
         super(GraphConvolution, self).__init__()
         self.in_features = in_features
         self.out_features = out_features
-        self.weight = Parameter(torch.FloatTensor(in_features, out_features))
-        if bias:
-            self.bias = Parameter(torch.FloatTensor(out_features))
-        else:
-            self.register_parameter('bias', None)
+        self.weight = Linear(in_features, out_features, bias=bias)
+        # self.weight = Parameter(torch.FloatTensor(in_features, out_features))
+        # if bias:
+        #     self.bias = Parameter(torch.FloatTensor(out_features))
+        # else:
+        #     self.register_parameter('bias', None)
         self.reset_parameters()
 
     def reset_parameters(self):
@@ -28,12 +30,14 @@ class GraphConvolution(Module):
             self.bias.data.uniform_(-stdv, stdv)
 
     def forward(self, input, adj):
-        support = torch.matmul(input, self.weight)
+        # support = torch.matmul(input, self.weight)
+        support = self.weight(input)
         output = torch.matmul(adj, support)
-        if self.bias is not None:
-            return output + self.bias
-        else:
-            return output
+        return output
+        # if self.bias is not None:
+        #     return output + self.bias
+        # else:
+        #     return output
 
     def __repr__(self):
         return self.__class__.__name__ + ' (' \
