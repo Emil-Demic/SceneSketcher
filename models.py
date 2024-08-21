45d3934f9aeac532d65228b3f5e696c1b62adfb7
Emil-Demic <77987406+Emil-Demic@users.noreply.github.com>
2024-08-21 14:54:50 +0200

    Update models.py

--- models.py
+++ models.py
@@ -20,12 +20,12 @@ class GCNAttention(nn.Module):
     def __init__(self, gcn_input_shape, gcn_output_shape):
         super(GCNAttention, self).__init__()
 
-        # self.image_bbox_extract_net = get_network("inceptionv3", num_classes=2048)
-        self.image_bbox_extract_net = resnext50_32x4d(weights=ResNeXt50_32X4D_Weights.DEFAULT)
-        self.image_bbox_extract_net.fc = Identity()
-        # self.global_image_extract_net = get_network("inceptionv3", num_classes=num_categories)
-        self.global_image_extract_net = resnext50_32x4d(weights=ResNeXt50_32X4D_Weights.DEFAULT)
-        self.global_image_extract_net.fc = Linear(2048, num_categories)
+        self.image_bbox_extract_net = get_network("inceptionv3", num_classes=2048)
+        # self.image_bbox_extract_net = resnext50_32x4d(weights=ResNeXt50_32X4D_Weights.DEFAULT)
+        # self.image_bbox_extract_net.fc = Identity()
+        self.global_image_extract_net = get_network("inceptionv3", num_classes=num_categories)
+        # self.global_image_extract_net = resnext50_32x4d(weights=ResNeXt50_32X4D_Weights.DEFAULT)
+        # self.global_image_extract_net.fc = Linear(2048, num_categories)
         self.X = nn.Parameter(torch.zeros((num_categories, num_categories), dtype=torch.float32))
         self.linear = nn.Linear(LOOP_NUM, 1)
         nn.init.constant_(self.X, 1e-6)
@@ -33,11 +33,6 @@ class GCNAttention(nn.Module):
         self.gc1 = GraphConvolution(gcn_input_shape, gcn_output_shape)
 
     def forward(self, image_list, label_list, bbox_list, img, adj, batch):
-        '''
-        image_list存根据bbox截取好的图像
-        label_list存根据bbox截取好的图像类别标签
-        category_list存类别对应的5维输入：bbox均值,个数
-        '''
         gcn_input = torch.zeros((img.shape[0], num_categories, LOOP_NUM, 2052), dtype=torch.float32, requires_grad=False, device=device)
         img_features = self.image_bbox_extract_net(image_list)
         full_features = torch.concat((img_features, bbox_list), dim=1)
